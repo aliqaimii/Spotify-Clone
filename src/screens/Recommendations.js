e10b2f8e0475579f0e53efe20958fc8dfddd834a
Ali Raza <aliraza@webhr.email>
2023-01-17 12:27:07 +0500

    remove extra code

--- src/screens/Recommendations.js
+++ src/screens/Recommendations.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, ScrollView } from "react-native";
 
+import { Text, View, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { GetAccessToken, GetRecommendations } from "../services/Api";
